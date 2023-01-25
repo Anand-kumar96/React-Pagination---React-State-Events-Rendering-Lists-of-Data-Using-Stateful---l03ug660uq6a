a24f9af08e6ef943c32cf9d588cec7a955f41bab
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-01-26 00:04:04 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { fetchPosts } from '../api/fetchPosts';
+import {Post} from './Post'
+import { PaginationButtonsList } from './PaginationButtonsList';
 
 const PostList = () => {
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const [posts , setPosts] = useState([]);
+    
+    useEffect(()=>{
+        setLoading(true)
+        fetchPosts(page,5).then(data => setPosts(data)).then(()=>{
+            setLoading(false)
+            //console.log("kkk")
+        })
+       
+        //console.log(data)
+
+        
+    },[page])
     
     return (
         <>
-
+        
+            {loading? <div className='loader'>Loading</div> : posts.map((post,index) => {
+                return (
+                    <Post key={index} post={post}/>
+                )
+            })}
+            {<PaginationButtonsList activePage={page} changePage={setPage}/>}
+            
         </>
     )
 }
